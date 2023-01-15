5100528bed9775938bcb6314fa2adba1718cbdc1
hvlxh <monsta22019@gmail.com>
2023-01-15 17:00:27 +0530

    Bot Token leaked

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,6 @@ export interface Configure {
 }
 
 export const config: Configure = {
-  token:
-    "OTg5NDY1MjMwODU5OTg5MDI0.Gle0K8.jSkMIxpPIkt4v7XmzFTsjTOyBlYCqJIsjkLwDM", // Bot Token
+  token: "", // Bot Token
   global: true, // If you gonna register the interactions globally
 };
